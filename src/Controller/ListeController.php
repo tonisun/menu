e5da59e958a32249427adc226cf8ed2852b30806
Toni Zeidler <toni.z@gmx.de>
2022-11-10 11:16:14 +0100

    ListeController add name: 'liste.'

--- src/Controller/ListeController.php
+++ src/Controller/ListeController.php
@@ -10,10 +10,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/liste')]
+#[Route('/liste', name: 'liste.')]
 class ListeController extends AbstractController
 {
-    #[Route('/', name: 'app_liste_index', methods: ['GET'])]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(ListeRepository $listeRepository): Response
     {
         return $this->render('liste/index.html.twig', [
@@ -21,7 +21,7 @@ class ListeController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_liste_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
     public function new(Request $request, ListeRepository $listeRepository): Response
     {
         $liste = new Liste();
@@ -31,7 +31,7 @@ class ListeController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $listeRepository->save($liste, true);
 
-            return $this->redirectToRoute('app_liste_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('liste.index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('liste/new.html.twig', [
@@ -40,7 +40,7 @@ class ListeController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_liste_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Liste $liste): Response
     {
         return $this->render('liste/show.html.twig', [
@@ -48,7 +48,7 @@ class ListeController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_liste_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Liste $liste, ListeRepository $listeRepository): Response
     {
         $form = $this->createForm(Liste1Type::class, $liste);
@@ -57,7 +57,7 @@ class ListeController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $listeRepository->save($liste, true);
 
-            return $this->redirectToRoute('app_liste_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('liste.index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('liste/edit.html.twig', [
@@ -66,13 +66,13 @@ class ListeController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_liste_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, Liste $liste, ListeRepository $listeRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$liste->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $liste->getId(), $request->request->get('_token'))) {
             $listeRepository->remove($liste, true);
         }
 
-        return $this->redirectToRoute('app_liste_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('liste.index', [], Response::HTTP_SEE_OTHER);
     }
 }
